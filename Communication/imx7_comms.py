be569dab2361dc336f2b0806197a858c971274e3
oloyedegbemiga <gbemigao@seas.upenn.edu>
2026-01-19 14:11:54 -0500

    IMX7 side communication completed.

--- Communication/imx7_comms.py
+++ Communication/imx7_comms.py
@@ -67,5 +67,55 @@ class IMX7Communication:
             'timestamp': time.time()
         }
         return json.dumps(data).encode('utf-8')
+    
+    def send_sensor_data(self, rpm, imu_data, encoder_data, tof_data):
+        """
+        send sensor data to Jetson
+        
+        """
+
+        packet = self.pack_sensor_data(rpm=rpm, imu_data=imu_data, encoder_data=encoder_data,tof_data=tof_data)
+        try:
+            self.send_sock.sendto(packet, (self.jetson_ip, self.send_port))
+        except Exception as e:
+            print(f"Error sending data: {e}")
+
+    def receive_commands(self):
+        while self.running:
+            try:
+                data, addr = self.recv_sock.recvfrom(4096)
+                cmd = json.loads(data.decode('utf-8'))
+                if 'rpm' in cmd:
+                    self.target_rpm = cmd['rpm']
+                    print(f"received rpm command: {self.target_rpm}")
+
+                if 'lcd' in cmd:
+                    self.lcd_text = cmd['lcd']
+                    print(f"received LCD text: {self.lcd_text}")
+
+                    # TODO: update LCD displays.
+
+                if 'led' in cmd:
+                    self.led_color = cmd['led']
+                    print(f"recieved LED color: {self.led_color}")
+                    # TODO: update LED displays. Perhaps convert to approp. format
+
+            except socket.timeout:
+                continue
+            except Exception as e:
+                print(f"Error receiving commands: {e}")
+
+    def start_receiver_thread(self):
+        """
+        start the command receiver in background thread
+        """
+        recv_thread = threading.Thread(target=self.receive_commands, daemon=True)
+        recv_thread.start()
+        return recv_thread
+    
+    def close(self):
+        self.running = False
+        self.send_sock.close()
+        self.recv_sock.close()
 
 
